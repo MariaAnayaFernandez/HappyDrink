adc3ec65dc7e1bbf5d16172c2cf57d1cf7519991
MariaAnaya <112909891+MariaAnayaFernandez@users.noreply.github.com>
2022-12-08 23:56:23 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,267 +1,200 @@
-//Array vacío
 
-const bebidasEnCuenta = []
+//Array 
 
+let cantidad = []
 
+let cuenta = []
 
 //Query Elementos
 
-const cardConteinerCla = document.querySelector("#drinksClasicos")
+const cardConteiner = document.getElementById("drinksCards")
 
-const cardConteinerEs = document.querySelector("#drinksEspeciales")
+const tablaCuenta = document.getElementById("table-cuenta")
 
-const cardConteinerTemp = document.querySelector("#drinksTemporada")
-
-const renglonNombre = document.querySelector('#renglonNombre')
-
-const renglonPrecio = document.querySelector('#renglonPrecio')
-
-const renglonCantidad = document.querySelector('#renglonCantidad')
+const ordenar = document.querySelector(".orden-fianl")
 
 
 
 //Funciones
 
 
+ordenar.addEventListener('click', () =>{
+    sweetAlert(  '¡Happy Drinks en preparación!',
+    'En un momento los recibirás en tu mesa',)
+    cuenta.length = 0
+    actualizarCuenta()
+
+})
+
+
 const mandarACuenta = (e) => {
-    const nombreCoctelElegido= (e.target.closest('.col').getAttribute('nombre'))
-    const coctelElegido = allDrinks.find((coctel) => coctel.nombre == nombreCoctelElegido) 
-    renglonNombre.textContent = coctelElegido.nombre
-    renglonPrecio.textContent = `$${coctelElegido.precio}`
-    renglonCantidad.textContent = 1
-    localStorage.setItem(coctelElegido)
+    const nombreCoctelElegido = (e.target.closest('.col').getAttribute('nombre'))
+    const coctelElegido = allDrinks.find((coctel) => coctel.nombre == nombreCoctelElegido)
+    const nuevoCoctelElegido ={
+        name:(coctelElegido.nombre),
+        price:(coctelElegido.precio),
+        cantidad: 1
+    }
+
+    addDrinkCuenta(nuevoCoctelElegido)
+}
+
+function addDrinkCuenta(nuevoCoctelElegido){
+
+    const inputCantidad=tablaCuenta.getElementsByClassName('input-cantidad')
+    for(let i =0; i<cuenta.length; i++){
+        if(cuenta[i].name.trim() === nuevoCoctelElegido.name.trim()){
+            cuenta[i].cantidad ++;
+            const inputValue = inputCantidad[i]
+            inputValue.value++
+            totalCuenta()
+            return null;
+        }
+    }
+    cuenta.push(nuevoCoctelElegido)
+   
+    //localStorage.setItem('cuenta', JSON.stringify(cuenta))
+    actualizarCuenta()
+}
+
+
+function actualizarCuenta () {
+    tablaCuenta.innerHTML= `                            
+    <thead>
+    <tr>
+      <th scope="col">Drink</th>
+      <th scope="col">Precio</th>
+      <th scope="col">Cantidad</th>
+    </tr>
+  </thead>
+  `;
+
+    cuenta.forEach((drink) => {
+
+        let actualizarTabla = document.createElement('tbody')
+        actualizarTabla.classList.add('tableBody')
+        actualizarTabla.innerHTML = `
+                <tr class=tablaContenido>
+                    <td class="nameCuenta">${drink.name}</td>
+                    <td>$${drink.price}</td>
+                    <td>
+                        <input type="number" min="1" value=${drink.cantidad} class="input-cantidad">
+                        <button class="w3-button delete">X</button>
+                    </td>
+                </tr>
+                
+        `
+        tablaCuenta.append(actualizarTabla)
+
+        actualizarTabla.querySelector(".delete").addEventListener('click', eliminarDrinkCuenta)
+        actualizarTabla.querySelector(".input-cantidad").addEventListener('change', addCantidad)
+    })
     
+    totalCuenta()
 }
 
-
-
-
-
-const menuClasicosCard = () => {
-    drinksClasicos.forEach((coctel) => {
-        let nuevaCardClas = document.createElement('div')
-        nuevaCardClas.classList.add('col')
-        nuevaCardClas.setAttribute('nombre', coctel.nombre)
-        nuevaCardClas.innerHTML = `    
-        <div class="menu-div">
-        <img src=${coctel.img} class="card-img-top" alt="Gin Old Fashion">
-        <div class="card-body">
-          <h5 class="card-title titulo-tarjeta"> ${coctel.nombre} </h5>
-          <p class="card-text">${coctel.ingredientes}</p>
-          <div class="ordenar">
-            <p class="cantidad">$${coctel.precio}.00</p>
-            <label class="cantidad">Cantidad:</label>
-            <input class="inp-ordenar" type=number min="1" max="20">
-            <button class="btn-ordenar">Ordenar</button>
-          </div>
-        </div>
-      </div>
-        
-  `
-        cardConteinerCla.append(nuevaCardClas)
-
+function totalCuenta(){
+    let total = 0;
+    const totalDrinks = document.querySelector(".total-cuenta")
+    cuenta.forEach((drink) => {
+        total = total + (drink.price*drink.cantidad)
     })
+    totalDrinks.innerHTML = `Total $${total}`
+    addLocalStorage()
+    sumar()
 }
-
-
-const menuEspecialesCard = () =>{
-
-    drinksEspeciales.forEach((coctel) => {
-        let nuevaCardEs = document.createElement('div')
-        nuevaCardEs.classList.add('col')
-        nuevaCardEs.setAttribute('nombre', coctel.nombre)
-        nuevaCardEs.innerHTML = `    
-        <div class="menu-div">
-        <img src=${coctel.img} class="card-img-top" alt="Gin Old Fashion">
-        <div class="card-body">
-          <h5 class="card-title titulo-tarjeta"> ${coctel.nombre} </h5>
-          <p class="card-text">${coctel.ingredientes}</p>
-          <div class="ordenar">
-            <p class="cantidad">$${coctel.precio}.00</p>
-            <label class="cantidad">Cantidad:</label>
-            <input class="inp-ordenar" type=number min="1" max="20">
-            <button class="btn-ordenar">Ordenar</button>
-          </div>
-        </div>
-      </div>
-        
-  `
-    cardConteinerEs.append(nuevaCardEs);
-    })
-}
-
-const menuTemporadaCard = () =>{
-    drinksTemporada.forEach((coctel) => {
-        let nuevaCardTemp = document.createElement('div')
-        nuevaCardTemp.classList.add('col')
-        nuevaCardTemp.setAttribute('nombre', coctel.nombre)
-        nuevaCardTemp.innerHTML = `    
-        <div class="menu-div">
-        <img src=${coctel.img} class="card-img-top" alt="Gin Old Fashion">
-        <div class="card-body">
-          <h5 class="card-title titulo-tarjeta"> ${coctel.nombre} </h5>
-          <p class="card-text">${coctel.ingredientes}</p>
-          <div class="ordenar">
-            <p class="cantidad">$${coctel.precio}.00</p>
-            <label class="cantidad">Cantidad:</label>
-            <input class="inp-ordenar" type=number min="1" max="20">
-            <button class="btn-ordenar">Ordenar</button>
-          </div>
-        </div>
-      </div>
-        
-  `
-    cardConteinerTemp.append(nuevaCardTemp);
-        
-    })
-
-    /*const cantidadElegida = document.querySelectorAll('.inp-prdenar')
-    cantidadElegida.forEach((value)=>{
-        value.addEventListener('input', cantidadDrinks)
-    })*/
     
-    const ordenarButton = document.querySelectorAll('.btn-ordenar')
+function eliminarDrinkCuenta(e){
+    const buttonDelete = e.target
+    const actualizarTabla = buttonDelete.closest(".tableBody")
+    const name = actualizarTabla.querySelector('.nameCuenta').textContent;
+    for(let i=0; i < cuenta.length ; i++){
+  
+      if(cuenta[i].name.trim() === name.trim()){
+        cuenta.splice(i, 1)
+      }
+    }
+    actualizarTabla.remove()
+    totalCuenta()
+    
+    }
+
+function addCantidad(e){
+    const sumaDrink = e.target
+    const actualizarTabla = sumaDrink.closest(".tableBody")
+    const name = actualizarTabla.querySelector('.nameCuenta').textContent
+    cuenta.forEach(drink => {
+        if (drink.name.trim() === name){
+            sumaDrink.value < 1 ? (sumaDrink.value = 1) : sumaDrink.value;
+            drink.cantidad = sumaDrink.value;
+            totalCuenta()
+        }
+    })
+}
+
+
+
+function sumar()
+{
+  const total = document.querySelector(".contador");
+  let subtotal = 0;
+  [ ...document.getElementsByClassName( "input-cantidad" ) ].forEach( function ( element ) {
+    if(element.value !== '') {
+      subtotal += parseFloat(element.value);
+    }
+  });
+  total.value = subtotal;
+  total.innerText = total.value
+}
+
+//Cards Menu
+
+const menu = () =>{
+    allDrinks.forEach((coctel) => {
+        let nuevaCard = document.createElement('div')
+        nuevaCard.classList.add('col')
+        nuevaCard.setAttribute('nombre', coctel.nombre)
+        nuevaCard.innerHTML = `    
+        <div class="menu-div">
+        <img src=${coctel.img} class="card-img-top" alt="Gin Old Fashion">
+        <div class="card-body">
+          <h5 class="card-title titulo-tarjeta"> ${coctel.nombre} </h5>
+          <p class="card-text">${coctel.ingredientes}</p>
+          <div class="ordenar">
+            <p class="cantidad">$${coctel.precio}.00</p>
+            <button id="${coctel.btnID}" class="btn-ordenar">Ordenar</button>
+          </div>
+        </div>
+      </div>
+        
+  `
+        cardConteiner.append(nuevaCard)
+
+    })
+
+
+    const ordenarButton = document.querySelectorAll(".btn-ordenar")
     ordenarButton.forEach((button) =>{
     button.addEventListener('click', mandarACuenta)
-})
-    
+    })
 }
+
+function addLocalStorage(){
+    localStorage.setItem('cuenta', JSON.stringify(cuenta))
+}
+
+window.onload = function(){
+    const storage = JSON.parse(localStorage.getItem('cuenta'));
+    if (storage){
+        cuenta = storage;
+        actualizarCuenta()
+    }
+}
+
+
 
 
 //Ejecuciones
 
-menuClasicosCard();
-
-menuEspecialesCard();
-
-menuTemporadaCard();
-
-
-//*************** Código de tareas anteriores ***************** 
-
-
-
-/*
-
-//Funciones:
-
-function casosClasicos(){
-    let clasicos = prompt ("Los Happy Drinks Clásicos son: " + (menuClasicos) + "Digita el número que deseas ordenar")
-switch (clasicos){ //menú de casos
-    case "1":
-        alert ("Ha pedido un " + menuClasicos[0]);
-        break;
-    case "2":
-        alert ("Ha pedido un " + menuClasicos[1]);
-        break;
-    case "3":
-        alert ("Ha pedido un " + menuClasicos[2]);
-        break;
-    case "4":
-        alert ("Ha pedido un " + menuClasicos[3]);
-        break;
-    case "5":
-        alert ("Ha pedido un " + menuClasicos[4]);
-        break;
-    case "6":
-        alert ("Ha pedido un " + menuClasicos[5]);
-        break;
-    default:
-        alert ("Valor incorrecto")
-    }
-};
-
-function casosEspecialidades(){
-    let especialidades = prompt ("Los Happy Drinks de la casa son: " + (menuEpecialidades) + "Digita el número que deseas ordenar")
-switch (especialidades){ //menú de casos
-    case "1":
-        alert ("Ha pedido un " + menuEpecialidades[0]);
-        break;
-    case "2":
-        alert ("Ha pedido un " + menuEpecialidades[1]);
-        break;
-    case "3":
-        alert ("Ha pedido un " + menuEpecialidades[2]);
-        break;
-    case "4":
-        alert ("Ha pedido un " + menuEpecialidades[3]);
-        break;
-    case "5":
-        alert ("Ha pedido un " + menuEpecialidades[4]);
-        break;
-    case "6":
-        alert ("Ha pedido un " + menuEpecialidades[5]);
-        break;
-    default:
-        alert ("Valor incorrecto")
-    }
-};
-
-function casosDeTemporada(){
-    let temporada = prompt ("Los Happy Drinks de la casa son: " + (menuTemporada) + "Digita el número que deseas ordenar")
-switch (temporada){ //menú de casos
-    case "1":
-        alert ("Ha pedido un " + menuTemporada[0]);
-        break;
-    case "2":
-        alert ("Ha pedido un " + menuTemporada[1]);
-        break;
-    case "3":
-        alert ("Ha pedido un " + menuTemporada[2]);
-        break;
-    case "4":
-        alert ("Ha pedido un " + menuTemporada[3]);
-        break;
-    case "5":
-        alert ("Ha pedido un " + menuTemporada[4]);
-        break;
-    case "6":
-        alert ("Ha pedido un " + menuTemporada[5]);
-        break;
-    default:
-        alert ("Valor incorrecto")
-    }
-};
-
-function menuPrincipal(){
-    let tipomenu = prompt ("Selecciona una opción de menú: 'Clásicos', 'Especialidades', o 'De temporada'").toLowerCase();
-    if(tipomenu == 'clasicos'){
-         casosClasicos();
-     } else if (tipomenu == 'especialidades'){
-         casosEspecialidades();
-     } else if (tipomenu == 'de temporada'){
-         casosDeTemporada();
-     } else{alert("Valor incorrecto")}
-}
-
-function agregarBebidas(){
-    let repetirMenu = prompt ("¿Desea otro Happy Drink? 1-sí , 2-no").toLowerCase();
-    if (repetirMenu == '1'){
-        menuPrincipal();
-    } else if (repetirMenu == '2'){
-        alert ("¡Gracias por su compra!")
-    } else {
-        alert ("Valor incorrecto")
-    }
-};
-
-
-//Ingreso restringido a mayores de edad:
-
-let edad = prompt("Digita tu edad");
-
-if (edad >= 18) {
-    alert ("¡Bienvenid@ a Happy Drink!")
-
-    menuPrincipal();
-
-    agregarBebidas();
-
-    alert ('Algunos de nuestros precios')
-    coctel1.mostrarPrecio()
-    coctel9.mostrarPrecio()
-    coctel15.mostrarPrecio()
-
-}else alert ("Lo sentimos, Happy Drink sólo es para mayores de edad."); */
-
+menu();
 
